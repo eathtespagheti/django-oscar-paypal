42e1bd4839d753c9208258452cfcc43d7db64b4b
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-10-08 12:00:38 +0200

    Add default auto field to remove django warning

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -141,6 +141,8 @@ TEMPLATES = [
     }
 ]
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
