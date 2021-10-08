3121551f505422cd62150c06bd8b7e15196d3b14
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-10-08 15:30:59 +0200

    Update with more permissive requirements

--- setup.py
+++ setup.py
@@ -17,13 +17,13 @@ setup(
     packages=find_packages(exclude=['sandbox*', 'tests*']),
     include_package_data=True,
     install_requires=[
-        'django>=2.2,<=3.2',
+        'django>=2.2,<3.3',
         'paypal-checkout-serversdk>=1.0.1',
         'requests>=2.26.0',
         'django-localflavor'
     ],
     extras_require={
-        'oscar': ['django-oscar>=2.0,<=3.1']
+        'oscar': ['django-oscar>=2.0,<3.2']
     },
     # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
